43bc36612f21e05c12eec46e72e5adb17699b699
hosenur <hosenur.dev@gmail.com>
2024-06-27 02:12:44 +0530

    make content client only

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+</script>
+
 <template>
     <main>
-      <ContentDoc />
+        <ClientOnly>
+
+            <ContentDoc />
+        </ClientOnly>
     </main>
   </template>
