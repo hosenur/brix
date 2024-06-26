d2677d8df23142b3c9a2f181bd280e6e72716ee7
hosenur <hosenur.dev@gmail.com>
2024-06-27 03:13:06 +0530

    idek

--- app.vue
+++ app.vue
@@ -1,9 +1,6 @@
-<script setup>
-  const nuxtApp = useNuxtApp();
-</script>
-
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-</template>
+    <div>
+      <NuxtPage />
+    </div>
+  </template>
+  
