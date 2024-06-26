e5066bb412db9b70728973fde7a2bc7b372fd876
hosenur <hosenur.dev@gmail.com>
2024-06-27 03:26:04 +0530

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/content"]  ,
   routeRules: {
-    '/**': { prerender: true }
+    '/': { prerender: true },
+    '/react': { prerender: true }
   }
 })
