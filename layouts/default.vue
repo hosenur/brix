795b92abe5764f78aaa781c7227327f86066a4bc
hosenur <hosenur.dev@gmail.com>
2024-06-26 23:23:05 +0530

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -87,9 +87,9 @@ const verticalLinks = computed(() => route.path.startsWith('/vue') ? verticalLin
             <ClientOnly>
                 <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'" color="gray"
                     variant="ghost" aria-label="Theme" @click="isDark = !isDark" />
-                <template #fallback>
+                <!-- <template #fallback>
                     <div class="w-8 h-8" />
-                </template>
+                </template> -->
             </ClientOnly>
         </nav>
     </div>
