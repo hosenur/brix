d1a1ebe6dfa2eebbb8f09c542984b48d52dc3149
hosenur <hosenur.dev@gmail.com>
2024-06-27 00:15:57 +0530

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,15 +19,15 @@ const links = [
     }
 ];
 
-const colorMode = useColorMode();
-const isDark = computed({
-    get() {
-        return colorMode.value === 'dark';
-    },
-    set(value) {
-        colorMode.preference = value ? 'dark' : 'light';
-    }
-});
+// const colorMode = useColorMode();
+// const isDark = computed({
+//     get() {
+//         return colorMode.value === 'dark';
+//     },
+//     set(value) {
+//         colorMode.preference = value ? 'dark' : 'light';
+//     }
+// });
 
 const verticalLinksVue = [
     {
@@ -84,6 +84,13 @@ const verticalLinks = computed(() => route.path.startsWith('/vue') ? verticalLin
             <Logo />
 
             <UHorizontalNavigation :links="links" class="basis-0" />
+            <!-- <ClientOnly>
+                <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'" color="gray"
+                    variant="ghost" aria-label="Theme" @click="isDark = !isDark" />
+                 <template #fallback>
+                    <div class="w-8 h-8" />
+                </template> 
+            </ClientOnly> -->
         </nav>
     </div>
     <div class="max-w-7xl mx-auto py-5 flex gap-5 font-onest">
